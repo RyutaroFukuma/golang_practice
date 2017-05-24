a40bbcfaf863b7e86eb01c1138f5dd730fc55e82
Ryutaro <ricken0203@gmail.com>
2017-05-24 21:15:34 +0900

    A Tour of Go

---
+++ slices-pointers.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+    names := [4]string{
+	"John",
+	"Paul",
+	"George",
+	"Ringo",
+    }
+    fmt.Println(names)
+
+    a := names[0:2]
+    b := names[1:3]
+    fmt.Println(a, b)
+
+    b[0] = "XXX"
+    fmt.Println(a, b)
+    fmt.Println(names)
+}
+
+//スライスはどんなデータも格納しておらず、単に元の配列の部分列を指し示している。
+//スライスの要素を変更すると、その元となる配列の対応する要素が変更される。
+//同じ元となる配列を共有している他のスライスは、それらの変更が反映される。
